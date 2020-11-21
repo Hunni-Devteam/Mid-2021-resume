f0ded357c67949252511437653b117b40039d9fd
Hunni <hunni.devteam@gmail.com>
2020-11-22 05:53:45 +0900

    initialize resume format

---
+++ src/resume/index.js
@@ -0,0 +1,16 @@
+
+// Consider about i18n / l10n ._.
+const Resume = {
+    title: '강훈',
+    description: 'Late 2020 CV', 
+    philosophies: [
+        {
+            title: 'Communication',
+            description: `
+좋은 소통은 상호 간의 신뢰에서 시작된다고 믿습니다.
+소통의 중요성을 인지하고, 서로 다른 백그라운드를 거쳐 온 팀원들을 믿고 존중하며 귀히 여깁니다.
+민첩하게 움직이는 개발 조직 내에서 개인이 갖는 책임의 무게를 알고, 진심을 다해 일합니다.
+            `
+        }
+    ],
+};
